cf65c9c556c2ee4d3d9ac9a682bbfad98388a1b4
Serhii-Kysil <serik.kisel000@gmail.com>
2024-06-23 18:45:37 +0300

    removing hours from date

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,11 +1,15 @@
 export const toUnixTimestamp = (date) => {
   if (!date) return null;
-  return Math.floor(new Date(date).getTime() / 1000);
+  const newDate = new Date(date);
+  newDate.setHours(0, 0, 0, 0);
+  return Math.floor(newDate.getTime() / 1000);
 };
 
 export const fromUnixTimestamp = (timestamp) => {
   if (!timestamp) return null;
-  return new Date(timestamp * 1000);
+  const date = new Date(timestamp * 1000);
+  date.setHours(0, 0, 0, 0);
+  return date;
 };
 
 export const getPreviousDay = (date) => {
